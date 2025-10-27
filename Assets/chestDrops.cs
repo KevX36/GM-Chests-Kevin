3c391008c0198786c8df2acc86d78b2ecbccb3fc
KevX36 <kevincard2007@gmail.com>
2025-10-27 11:13:33 -0300

    Initial commit

---
+++ Assets/chestDrops.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class chestDrops : MonoBehaviour
+{
+    
+    public int woodDropRate;
+    public int bronzeDropRate;
+    public int sliverDropRate;
+    public int goldDropRate;
+    public int platnumDropRate;
+
+
+    public int woodCommon;
+    public int woodUncommon;
+    public int woodRare;
+    public int woodEpic;
+    public int woodLegend;
+
+
+    public int bronzeCommon;
+    public int bronzeUncommon;
+    public int bronzeRare;
+    public int bronzeEpic;
+    public int bronzeLegend;
+
+
+    public int sliverCommon;
+    public int sliverUncommon;
+    public int sliverRare;
+    public int sliverEpic;
+    public int sliverLegend;
+
+
+    public int goldCommon;
+    public int goldUncommon;
+    public int goldRare;
+    public int goldEpic;
+    public int goldLegend;
+
+
+    public int platnumCommon;
+    public int platnumUncommon;
+    public int platnumRare;
+    public int platnumEpic;
+    public int platnumLegend;
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
